b02c2e701349436c856eab9c444d3ad06cfc128b
alirafi3811-star <alirafi3811@gmail.com>
2026-07-12 18:53:30 +0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,117 @@
+const menu = [
+
+{
+category:"BURGERS",
+name:"Juicy Cheeseburger",
+price:8.90
+},
+
+{
+category:"BURGERS",
+name:"Jalapeno Boom Burger",
+price:10.50
+},
+
+{
+category:"BURGERS",
+name:"Super Healthy Veggie Burger",
+price:9.90
+},
+
+{
+category:"BURGERS",
+name:"Crispy Crown Chicken Burger",
+price:8.90
+},
+
+{
+category:"BURGERS",
+name:"Hawaiian Burger",
+price:12.50
+},
+
+{
+category:"PIZZA",
+name:"Margherita",
+price:8.90
+},
+
+{
+category:"PIZZA",
+name:"Veggie-Loaded",
+price:9.90
+},
+
+{
+category:"PIZZA",
+name:"Mushroom",
+price:9.50
+},
+
+{
+category:"PIZZA",
+name:"Mafiozza",
+price:11.50
+},
+
+{
+category:"PIZZA",
+name:"Quatro Formaggi",
+price:9.90
+},
+
+{
+category:"PIZZA",
+name:"Pollo Pizza",
+price:10.90
+},
+
+{
+category:"PASTA",
+name:"Aglio e Olio",
+price:7.50
+},
+
+{
+category:"PASTA",
+name:"Al Pesto",
+price:8.50
+},
+
+{
+category:"PASTA",
+name:"Bolognese",
+price:9.00
+},
+
+{
+category:"PASTA",
+name:"Pollo La Favorita",
+price:8.90
+},
+
+{
+category:"PASTA",
+name:"Gorgonzola",
+price:9.90
+},
+
+{
+category:"SNACKS",
+name:"Chicken Wings",
+price:5.50
+},
+
+{
+category:"SNACKS",
+name:"Loaded Nachos",
+price:4.90
+},
+
+{
+category:"SNACKS",
+name:"French Fries",
+price:3.90
+}
+
+];
